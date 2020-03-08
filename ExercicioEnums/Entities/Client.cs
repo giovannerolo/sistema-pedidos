8729800888a502eea311f41e73973d963dd25457
Giovanne <giovannerolo@outlook.com>
2020-03-08 17:07:53 -0300

    Projeto e um sistema de pedidos em C#, exercicio usando a classe enums

---
+++ ExercicioEnums/Entities/Client.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioEnums.Entities
+{
+    class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDay { get; set; }
+
+        public Client()
+        {
+        }
+
+        public Client(string name, string email, DateTime birthDay)
+        {
+            Name = name;
+            Email = email;
+            BirthDay = birthDay;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", (" + BirthDay.ToString("dd/MM/yyyy")
+                + ") - " + Email;
+        }
+    }
+}
